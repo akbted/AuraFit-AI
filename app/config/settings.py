ce98c835fe1d90411a35d9c5a7e0ae050c2a6324
Anantha Krishna B <157831286+akbted@users.noreply.github.com>
2025-12-29 00:42:16 +0530

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import List, Optional
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
 
 class Settings(BaseSettings):
     
@@ -11,8 +14,20 @@ class Settings(BaseSettings):
     API_PORT: int = 8000
     CORS_ORIGINS: list = ["http://localhost:3000", "http://localhost:5173"]
     
-    class Config:
-        env_file = ".env"
+    hf_token: str = ""  # Add this field
+
+    # Fetch variables
+    DB_USER: str 
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",           
+        env_file_encoding="utf-8",
+        extra="ignore"            
+    )
     
 
 
